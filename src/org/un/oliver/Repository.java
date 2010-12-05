a4c72017fa600c45a301a476bc03cc1de8c09b9f
Graham Brooks <graham@grahambrooks.com>
2010-12-05 12:29:02 -0500

    Added Showing a datasource that has been created

--- src/org/un/oliver/Repository.java
+++ src/org/un/oliver/Repository.java
@@ -3,26 +3,30 @@ package org.un.oliver;
 import com.google.appengine.api.datastore.*;
 
 public class Repository {
-    private final DatastoreService datastoreService;
+	private final DatastoreService datastoreService;
 
-    public Repository() {
-        this(DatastoreServiceFactory.getDatastoreService());
-    }
+	public Repository() {
+		this(DatastoreServiceFactory.getDatastoreService());
+	}
 
-    public Repository(DatastoreService datastoreService) {
-        this.datastoreService = datastoreService;
-    }
+	public Repository(DatastoreService datastoreService) {
+		this.datastoreService = datastoreService;
+	}
 
-    public Key store(Entity entity) {
-        Key result = datastoreService.put(entity);
-        return result;
-    }
+	public Key store(Entity entity) {
+		Key result = datastoreService.put(entity);
+		return result;
+	}
 
-    public Entity get(Key key) {
-        try {
-            return datastoreService.get(key);
-        } catch (EntityNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	public Entity get(String kind, long id) {
+		return get(KeyFactory.createKey(kind, id));
+	}
+
+	public Entity get(Key key) {
+		try {
+			return datastoreService.get(key);
+		} catch (EntityNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
