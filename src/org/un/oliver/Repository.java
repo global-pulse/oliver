b470e03bcf47a1bd2a26cd50a801bf9d6680be1d
dnchen <daphnec@daphnec-macbookpro.local>
2010-12-05 12:54:01 -0500

    A hack to display all data sources

--- src/org/un/oliver/Repository.java
+++ src/org/un/oliver/Repository.java
@@ -1,9 +1,20 @@
 package org.un.oliver;
 
-import com.google.appengine.api.datastore.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 
 public class Repository {
-    private final DatastoreService datastoreService;
+	private final DatastoreService datastoreService;
 
     public Repository() {
         this(DatastoreServiceFactory.getDatastoreService());
@@ -24,5 +35,26 @@ public class Repository {
         } catch (EntityNotFoundException e) {
             throw new RuntimeException(e);
         }
+    }
+    
+    public List<Entity> findAllDataSources() {
+		List<Entity> entityList = new ArrayList();
+		boolean moreEntitiesExist = true;
+		int keyEntityId = 1;
+		while (moreEntitiesExist) {
+			Key key = KeyFactory.createKey("DataSource", keyEntityId);
+			try {
+				Entity entity = datastoreService.get(key);
+				entityList.add(entity);
+			} catch (EntityNotFoundException e) {
+				moreEntitiesExist = false;
+			}
+			keyEntityId++;
+		}
+    	return entityList;
+    }
+    
+    private PersistenceManager getPersistenceManager() {
+    	return JDOHelper.getPersistenceManagerFactory("transactions-optional").getPersistenceManager();
     }
 }
---
+++ war/datasource/list.jsp
@@ -0,0 +1,33 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="com.google.appengine.api.datastore.Entity" %>
+<%@ page import="java.util.List" %>
+<%@ page import="org.un.oliver.Repository" %>
+
+<html>
+  <body>
+
+These are the data stores and feeds that we know about.If we've missed one, please <a href="/datasource/new.jsp">tell us about it</a>.
+
+<table>
+  <tr>
+    <th>Name</th>
+    <th>Available from...</th>
+    <th>Tags</th>
+  </tr>
+  <%
+    Repository repo = new Repository();
+    List<Entity> datasources = repo.findAllDataSources();
+    for (Entity datasource : datasources) {
+      %>
+      <tr>
+        <td><%= datasource.getProperty("name") %></td>
+        <td><%= datasource.getProperty("address") %></td>
+        <td><%= datasource.getProperty("tags") %></td>
+      </tr>
+      <%
+    }
+  %>
+</table>
+
+  </body>
+</html>
