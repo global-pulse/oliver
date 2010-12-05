7d7c935b3a663494c672f5c22fda9d4809a52c71
dnchen <daphnec@daphnec-macbookpro.local>
2010-12-05 13:10:32 -0500

    Failed merge - adding back get entity by kind queries

--- src/org/un/oliver/Repository.java
+++ src/org/un/oliver/Repository.java
@@ -16,27 +16,31 @@ import com.google.appengine.api.datastore.KeyFactory;
 public class Repository {
 	private final DatastoreService datastoreService;
 
-    public Repository() {
-        this(DatastoreServiceFactory.getDatastoreService());
-    }
+	public Repository() {
+		this(DatastoreServiceFactory.getDatastoreService());
+	}
 
-    public Repository(DatastoreService datastoreService) {
-        this.datastoreService = datastoreService;
-    }
+	public Repository(DatastoreService datastoreService) {
+		this.datastoreService = datastoreService;
+	}
 
-    public Key store(Entity entity) {
-        Key result = datastoreService.put(entity);
-        return result;
-    }
+	public Key store(Entity entity) {
+		Key result = datastoreService.put(entity);
+		return result;
+	}
 
-    public Entity get(Key key) {
-        try {
-            return datastoreService.get(key);
-        } catch (EntityNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    
+	public Entity get(String kind, long id) {
+		return get(KeyFactory.createKey(kind, id));
+	}
+
+	public Entity get(Key key) {
+		try {
+			return datastoreService.get(key);
+		} catch (EntityNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
     public List<Entity> findAllDataSources() {
 		List<Entity> entityList = new ArrayList();
 		boolean moreEntitiesExist = true;
